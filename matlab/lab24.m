fad16f90007e54f803fb3b0d755ece755aa9425b
orestisf <orestisf1993@gmail.com>
2017-05-25 00:19:33 +0300

    Use cell(3, 1) for th_ref in lab24.m

--- matlab/lab24.m
+++ matlab/lab24.m
@@ -2,19 +2,19 @@ z = 1.1;
 t_s = 1;
 
 omega = 246.93; % -- rad/s
-th_ref = @(t) (5 + 2 * sin(omega*t));
+th_ref{1} = @(t) (5 + 2 * sin(omega*t));
 print_position
-lab2(a, z, t_s, th_ref);
-plot_results(th_ref);
+lab2(a, z, t_s, th_ref{1});
+plot_results(th_ref{1});
 
 omega = 10;
-th_ref = @(t) (5 + 2 * sin(omega*t));
+th_ref{2} = @(t) (5 + 2 * sin(omega*t));
 print_position
-lab2(a, z, t_s, th_ref);
-plot_results(th_ref);
+lab2(a, z, t_s, th_ref{2});
+plot_results(th_ref{2});
 
 omega = 3;
-th_ref = @(t) (5 + 2 * sin(omega*t));
+th_ref{3} = @(t) (5 + 2 * sin(omega*t));
 print_position
-lab2(a, z, t_s, th_ref);
-plot_results(th_ref);
+lab2(a, z, t_s, th_ref{3});
+plot_results(th_ref{3});
